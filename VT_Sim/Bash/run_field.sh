6e0e0de8d55ec2f78770923e030a65bb35a9de6c
Barbara <babialmeida1508@gmail.com>
2017-09-15 23:11:05 -0300

    add add plugin ao bash

--- VT_Sim/Bash/run_field.sh
+++ VT_Sim/Bash/run_field.sh
@@ -10,5 +10,8 @@ export GAZEBO_RESOURCE_PATH=$(pwd)/Textures:$GAZEBO_RESOURCE_PATH
 # adicionar caminho para o modelo
 export GAZEBO_MODEL_PATH=$(pwd)/Models:$GAZEBO_MODEL_PATH
 
+# adicionar caminho para o plugin
+export GAZEBO_PLUGIN_PATH=$(pwd)/Plugins/VT_sim/import:${GAZEBO_PLUGIN_PATH}
+
 # iniciar gazebo com mundo parado
 gazebo Worlds/vt_bowlfield.world -u
